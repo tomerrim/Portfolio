18a98ec19974a16a30efc6791e0ea50360cd8692
Tomer <tomerrim@gmail.com>
2023-05-25 23:57:04 +0300

    added function on Header.jsx to translate

--- src/features/Header.jsx
+++ src/features/Header.jsx
@@ -2,25 +2,36 @@ import { Flex, HStack, Image, Link } from "@chakra-ui/react";
 import logoImg from "@/assets/images/logo.png";
 import bubbleImg from "@/assets/images/bubble.png";
 import flagENImg from "@/assets/images/flag-en.png";
+import flagIlImg from "@/assets/images/flag-il.png";
 import { useTranslation } from "react-i18next";
 
-
 export const Header = () => {
-  const {t} = useTranslation("home");
-    return (
-      <Flex justify={"space-between"}>
-        <Image src={logoImg} h={10} />
-        <HStack>
-          <Image src={bubbleImg} h={10} />
-          <Link
-            href="mailto:tomerrim@gmail.com?subject-Cantacting you from your portfolio"
-            fontSize={"lg"}
-            fontWeight={"bold"}
-          >
-            {t("hireMe")}
-          </Link>
-          <Image pl={20} src={flagENImg} h={8} />
-        </HStack>
-      </Flex>
-    );
-}
+  const { t, i18n } = useTranslation("home");
+
+  const switchLanguage = () => {
+    i18n.changeLanguage(i18n.language === "en" ? "il" : "en");
+  };
+
+  return (
+    <Flex justify={"space-between"}>
+      <Image src={logoImg} h={10} />
+      <HStack>
+        <Image src={bubbleImg} h={10} />
+        <Link
+          href="mailto:tomerrim@gmail.com?subject-Cantacting you from your portfolio"
+          fontSize={"lg"}
+          fontWeight={"bold"}
+        >
+          {t("hireMe")}
+        </Link>
+        <Image
+          onClick={switchLanguage}
+          pl={20}
+          src={i18n.language === "en" ? flagENImg : flagIlImg}
+          h={8}
+          cursor={"pointer"}
+        />
+      </HStack>
+    </Flex>
+  );
+};
--- src/locals/i18n-config.js
+++ src/locals/i18n-config.js
@@ -2,15 +2,19 @@ import { initReactI18next } from "react-i18next";
 import homeEN from "@/locals/en/home.json";
 import homeIL from "@/locals/il/home.json";
 import i18next from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-i18next.use(initReactI18next).init({
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
     resources: {
-        en: {
-            home: homeEN,
-        },
-        il: {
-            home: homeIL,
-        },
+      en: {
+        home: homeEN,
+      },
+      il: {
+        home: homeIL,
+      },
     },
     fallbackLng: "en",
-});
+  });
