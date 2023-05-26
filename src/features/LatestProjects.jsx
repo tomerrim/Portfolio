6d1c7938ea1f28b860036126695d6624babeb938
Tomer <tomerrim@gmail.com>
2023-05-26 14:06:46 +0300

    added latest projects section

---
+++ src/features/LatestProjects.jsx
@@ -0,0 +1,78 @@
+import { ImageSlider } from "@/components/imageSlider/ImageSlider";
+import { Box, Flex, Heading, Wrap, WrapItem,Text } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+
+const FAKE_PROJECTS = [
+  {
+    id: 1,
+    title: "Project 1",
+    description: "A project about this",
+    technologies: ["React", "ES6", "HTML", "CSS"],
+    images: [
+      "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+      "https://images.unsplash.com/photo-1444525873963-75d329ef9e1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    ],
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    description: "A project about that",
+    technologies: ["React", "Redux", "HTML", "CSS", "ES6"],
+    images: [
+      "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
+      "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    ],
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    description: "A project about things",
+    technologies: [
+      "React",
+      "Redux",
+      "HTML",
+      "CSS",
+      "ES6",
+      "Vercel",
+      "Bootstrap",
+    ],
+    images: [
+      "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    ],
+  },
+];
+
+export const LatestProjects = () => {
+    const { t } = useTranslation(["home"]);
+    const [projects, setProjects] = useState(FAKE_PROJECTS);
+
+    const renderProject = ({ id,images, title, description }) => {
+    return (
+      <WrapItem key={id} flexDir={"column"}>
+        <ImageSlider imageList={images} />
+        <Heading size={"md"} color={"secondary"} mt={3}>
+          <Box
+            display={"inline-block"}
+            bg={"primary.dark"}
+            w={25}
+            h={1}
+            mr={3}
+            verticalAlign={"middle"}
+          />
+          {title}
+        </Heading>
+        <Text>{description}</Text>
+      </WrapItem>
+    );
+  };
+
+  return (
+    <Flex direction={"column"} w={"100%"}>
+      <Heading>{t("latestProjects")}</Heading>
+      <Wrap mt={10} spacing={16}>
+        {projects?.map(renderProject)}
+      </Wrap>
+    </Flex>
+  );
+};
